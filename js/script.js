15859fa5d4bd97c652a8f3377bd3a7bc910b324b
fedemoretto11 <fedemoretto94@gmail.com>
2023-05-11 11:53:31 -0300

    Modiciacion Script cambio fondo

--- js/script.js
+++ js/script.js
@@ -3,10 +3,6 @@ let proximaFiesta = "Fiesta Nacional del Carnaval de la Amistad 2023"
 let pieFiesta = "La Fiesta ya comenzo! Te esperamos en Maipu!"
 const fechaObjetivo = new Date("04/15/2023 23:00:00")
 
-// VARIABLES CAMBIO DE FONDO
-const principal = document.querySelector(".seccion-principal");
-var fotos = ["img/verde-1.jpg", "img/rojo-1.jpg", "img/varios.jpg","img/viejas-epocas.jpg","img/batucada-1.jpg"] ;
-var index = 0;
 
 // VARIABLES CUENTA REGRESIVA
 const titulo = document.querySelector(".titulo-cuenta-regresiva");
@@ -26,14 +22,17 @@ titulo.innerHTML = proximaFiesta;
 pie.innerHTML = pieFiesta
 
 
-
-
 // Cambio de fondo 
-
+var index = 0;
 function cambiarFondo() {
-    principal.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(${fotos[index]})`;
-    index++
-    if (index == 5) {index = 0}   
+  const principal = document.querySelector(".seccion-principal");
+  fetch("../json/imagenesCarrousel.json")
+    .then(response => response.json())
+    .then(data => {
+      principal.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(${data[index].enlace})`
+      });
+  index++
+  if (index == 5) {index = 0}   
 }
 
 
@@ -41,18 +40,18 @@ function cambiarFondo() {
 
 
 function setDate() {
-    const now = new Date()
-    const duration = fechaObjetivo - now
+  const now = new Date()
+  const duration = fechaObjetivo - now
 
-    const diasRestantes = (Math.floor(duration / milisecondsOfADay)).toString().padStart(2,"0");
-    const horasRestantes = (Math.floor((duration % milisecondsOfADay) / milisecondsOfAnHour)).toString().padStart(2,"0");
-    const minutosRestantes = (Math.floor((duration % milisecondsOfAnHour) / milisecondsOfAMinute)).toString().padStart(2,"0");
-    const segundosRestantes = (Math.floor((duration % milisecondsOfAMinute) / milisecondsOfASecond)).toString().padStart(2,"0");
+  const diasRestantes = (Math.floor(duration / milisecondsOfADay)).toString().padStart(2,"0");
+  const horasRestantes = (Math.floor((duration % milisecondsOfADay) / milisecondsOfAnHour)).toString().padStart(2,"0");
+  const minutosRestantes = (Math.floor((duration % milisecondsOfAnHour) / milisecondsOfAMinute)).toString().padStart(2,"0");
+  const segundosRestantes = (Math.floor((duration % milisecondsOfAMinute) / milisecondsOfASecond)).toString().padStart(2,"0");
 
-    dias.textContent = diasRestantes <= 0 ? 00.toString().padStart(2,"00") : diasRestantes;
-    horas.textContent = horasRestantes <= 0 ? 00.toString().padStart(2,"00") : horasRestantes;
-    minutos.textContent = minutosRestantes <= 0 ? 00.toString().padStart(2,"00") : minutosRestantes;
-    segundos.textContent = segundosRestantes <= 0 ? 00.toString().padStart(2,"00") : segundosRestantes;
+  dias.textContent = diasRestantes <= 0 ? 00.toString().padStart(2,"00") : diasRestantes;
+  horas.textContent = horasRestantes <= 0 ? 00.toString().padStart(2,"00") : horasRestantes;
+  minutos.textContent = minutosRestantes <= 0 ? 00.toString().padStart(2,"00") : minutosRestantes;
+  segundos.textContent = segundosRestantes <= 0 ? 00.toString().padStart(2,"00") : segundosRestantes;
 }
 
 
